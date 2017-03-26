2211c5730aa0d2b2dbda57c5639d34c164e08da8
Nick Strupat <Nick@Nicks-MacBook-Pro.local>
2017-03-26 11:11:24 -0400

    fix macOS code

--- OSX.cs
+++ OSX.cs
@@ -7,13 +7,14 @@ namespace NickStrupat
     {
         public static Int32 GetSize()
         {
-            int lineSize;
-            IntPtr sizeOfLineSize = (IntPtr) sizeof(int);
+            IntPtr lineSize;
+            IntPtr sizeOfLineSize = (IntPtr) IntPtr.Size;
             sysctlbyname("hw.cachelinesize", out lineSize, ref sizeOfLineSize, IntPtr.Zero, IntPtr.Zero);
-            return lineSize;
+            return lineSize.ToInt32();
         }
 
         [DllImport("libc")]
-        private static extern int sysctlbyname(string name, out int int_val, ref IntPtr length, IntPtr newp, IntPtr newlen);
+        private static extern int sysctlbyname(string name, out IntPtr oldp, ref IntPtr oldlenp, IntPtr newp, IntPtr newlen);
+        // int sysctlbyname(const char *name, void *oldp, size_t *oldlenp, void *newp, size_t newlen);
     }
 }
