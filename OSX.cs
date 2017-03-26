faa991ce4d9540d42624416db97aed4431f8a539
Nick Strupat <Nick@Nicks-MacBook-Pro.local>
2017-03-26 11:15:07 -0400

    remove libc function signature comment

--- OSX.cs
+++ OSX.cs
@@ -15,6 +15,5 @@ namespace NickStrupat
 
         [DllImport("libc")]
         private static extern int sysctlbyname(string name, out IntPtr oldp, ref IntPtr oldlenp, IntPtr newp, IntPtr newlen);
-        // int sysctlbyname(const char *name, void *oldp, size_t *oldlenp, void *newp, size_t newlen);
     }
 }
